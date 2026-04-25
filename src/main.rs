7120ad98c230f14c32912bf30318aee0cc59edfb
Rory Mulligan <rorticus@gmail.com>
2026-04-25 18:29:59 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,12 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number = number_string.parse::<usize>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
         serde_json::Value::String(string.to_string())
+    } else if encoded_value.chars().next().unwrap() == 'i' {
+        // encoded value starts with an i its an integer
+        let e_index = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..e_index];
+        let number = number_string.parse::<i64>().unwrap();
+        serde_json::Value::Number(serde_json::Number::from(number))
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -32,5 +38,26 @@ fn main() {
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decode_integer() {
+        let value = "i54e";
+        let result = decode_bencoded_value(&value);
+        assert!(result.is_i64());
+        assert_eq!(result.as_i64().unwrap(), 54);
+    }
+
+    #[test]
+    fn test_decode_negative_integer() {
+        let value = "i-54e";
+        let result = decode_bencoded_value(&value);
+        assert!(result.is_i64());
+        assert_eq!(result.as_i64().unwrap(), -54);
     }
 }
