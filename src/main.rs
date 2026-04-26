4bbad52d48faa966a2ef6899d2362aabed01eb9d
Rory Mulligan <rorticus@gmail.com>
2026-04-26 17:19:02 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 
+use crate::torrent::parse_torrent;
+
 mod bencode;
+mod torrent;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,6 +16,13 @@ fn main() {
         let encoded_value = &args[2];
         let decoded_value = bencode::decode_bencoded_value(encoded_value).unwrap();
         println!("{}", decoded_value.to_string());
+    } else if command == "info" {
+        // read the file
+        let bytes = std::fs::read(&args[2]).unwrap();
+        let manifest = parse_torrent(&bytes).unwrap();
+
+        println!("Tracker URL: {}", manifest.announce);
+        println!("Length: {}", manifest.info.length);
     } else {
         println!("unknown command: {}", args[1])
     }
---
+++ src/torrent.rs
@@ -0,0 +1,39 @@
+use serde;
+use serde_json;
+
+use crate::bencode::decode_bencoded_value;
+
+#[derive(Debug, serde::Deserialize)]
+pub struct TorrentInfo {
+    pub length: i64,
+    pub name: String,
+    #[serde(rename = "piece length")]
+    pub piece_length: i64,
+    pub pieces: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct SingleTorrentManifest {
+    pub announce: String,
+    pub info: TorrentInfo,
+}
+
+pub fn parse_torrent(bytes: &[u8]) -> Result<SingleTorrentManifest, Box<dyn std::error::Error>> {
+    let value = decode_bencoded_value(&std::str::from_utf8(bytes)?)?;
+
+    return Ok(serde_json::from_value(value)?);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_single_file() {
+        let single_file_torrent = "d8:announce41:http://bttracker.debian.org:6969/announce4:infod6:lengthi678301696e4:name25:debian-503-amd64-CD-1.iso12:piece lengthi262144e6:pieces0:ee";
+
+        let result = parse_torrent(single_file_torrent.as_bytes()).unwrap();
+
+        assert_eq!(result.announce, "http://bttracker.debian.org:6969/announce");
+    }
+}
